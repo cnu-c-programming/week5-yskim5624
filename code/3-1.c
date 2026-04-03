481eb1bcb77a62c3936931c612a3aeff2fff3e9d
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-04-03 10:18:45 +0900

    Update 3-1.c

---
+++ code/3-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void read_only(const int* arr, int size) {
+
+    //arr[0] = 999;
+
+    for(int i=0; i<size; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+}
+
+int main() {
+    int nums[] = {10, 20, 30, 40, 50};
+    read_only(nums, 5);
+    return 0;
+}
