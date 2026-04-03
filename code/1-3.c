e68ecb686b6ce739a2a2da0fad68b931d3ca8920
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-04-03 09:53:28 +0900

    Add function to print array in reverse order

---
+++ code/1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_reverse(int *arr, int size) {
+    int *p = arr + size - 1;
+
+    while(p >= arr) {
+        if(p == arr) printf("%d", *p);
+        else {
+            printf("%d ", *p);
+        }
+        p--;
+    }
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 4, 5};
+
+    print_reverse(nums, 5);
+    printf("\n");
+    return 0;
+}
